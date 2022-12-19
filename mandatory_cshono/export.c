8df7a26b28d1be230524dbf4ae3e92fc06c7ddc2
Chihiro Shono <cshono@c1r26s3.42tokyo.jp>
2022-12-19 17:11:55 +0900

    commit

--- mandatory_cshono/export.c
+++ mandatory_cshono/export.c
@@ -6,7 +6,7 @@
 /*   By: cshono <cshono@student.42.tokyo>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 21:11:04 by cshono            #+#    #+#             */
-/*   Updated: 2022/12/14 15:54:21 by cshono           ###   ########.fr       */
+/*   Updated: 2022/12/19 17:11:06 by cshono           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,51 @@
 	
 // }
 
-char	*check_export(char *str)//コマンド内の記号エラー確認
-{
-	size_t i;
+// char	*check_export(char *str)//コマンド内の記号エラー確認
+// {
+// 	size_t i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (!ft_isalnum((str[i]))//コマンド内の記号エラー確認
-		{
-			printf("export: '%s': not a valid identifier\n",str);
-			free(str);
-			return (NULL);
-		}
-		i++;
-		//if//コマンド内の空白(スペース)確認
-	}
-	return (str);
+// 	i = 0;
+// 	while (str[i] != '\0')
+// 	{
+// 		if (!ft_isalnum((str[i]))//コマンド内の記号エラー確認
+// 		{
+// 			printf("export: '%s': not a valid identifier\n",str);
+// 			free(str);
+// 			return (NULL);
+// 		}
+// 		i++;
+// 		//if//コマンド内の空白(スペース)確認
+// 	}
+// 	return (str);
+// }
+
+char    *ft_strtok(char *str, char sepa)
+{
+    static char *stock = NULL;
+    char        *ptr;
+    int     i;
+
+    i = 0;
+    ptr = NULL;
+    if (str != NULL)
+        stock = ft_strdup(str);
+    while (*stock != '\0')
+    {
+        if (i == 0 && *stock != sepa)
+        {
+            i = 1;
+            ptr = stock;
+        }
+        else if (i == 1 && *stock == sepa)
+        {
+            *stock = '\0';
+            stock += 1;
+            break ;
+        }
+        stock++;
+    }
+    return (ptr);
 }
 
 void	export(char *line, t_env *env)//pwd=chihiro, t_env
@@ -53,22 +81,22 @@ void	export(char *line, t_env *env)//pwd=chihiro, t_env
 	char *str;
 	t_env *tmp;
 
+	tmp = NULL;
 	i = 0;
 	if (!ft_isalpha(line[i]));//コマンドラインの先頭の文字が数字の場合のエラー処理。
 		return;
-	if (!ft_strchr(line, '='));//コマンドラインに0が含まれていなかった場合のエラー処理
+	if (!ft_strchr(line, '='));//コマンドラインに=が含まれていなかった場合のエラー処理
 		return;
-	//tmp = get_env(line, get_env)
-	str = (char *)malloc(sizeof(char)*ft_strlen(line));
-	while (line[i] != '=')
+	str = ft_strtok(line, '=');
+	tmp = get_env(str, get_env);//あればそこのアドレスが返ってくる。NULLだと
+	if(!tmp)
 	{
-		str[i] = line[i];
-		i++;
-	}//mallocでメモリを確保する必要あり。
-	str[i] = '\0';
-	str = check_export(str);
-	// tmp = get_env(str, env);//同じenvがあればその場所のポインタを返してくれる。
-	// if(!tmp)//同じenvがなかった場合
-	// 	node_add_back(env, tmp);
-	//free(tmp);
+		tmp->var = str;
+		node_add_back(env,tmp);
+	}
+	if (tmp)
+	{
+		tmp->str = str;
+	}
+	free(str);
 }

c871ca26540302b71e5d0660d056ca82acb268dd
maria <winterzaq1@gmail.com>
2023-09-03 20:53:09 +0900

    redirect not working as intended

--- srcs/redirect/heredoc.c
+++ srcs/redirect/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 20:30:10 by myoshika          #+#    #+#             */
-/*   Updated: 2023/09/03 07:55:40 by myoshika         ###   ########.fr       */
+/*   Updated: 2023/09/03 20:49:29 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,15 +58,16 @@ static void	readline_heredoc_loop(int fd, char *delimiter)
 	}
 }
 
-void	delete_tmp_file(int fd, char *fd_name)
+static int	delete_tmp_file(int fd, char *fd_name)
 {
 	close(fd);
-	fd = open(fd_name, O_RDONLY); //why did i do this i cant remember im sleepy :(
+	fd = open(fd_name, O_RDONLY);
 	if (fd == -1)
 		print_error_and_exit("open failure");
 	if (unlink(fd_name) == -1)
 		print_error_and_exit("unlink failure");
 	free(fd_name);
+	return (fd);
 }
 
 int	set_up_heredoc(t_redir *redir, t_data *d)
@@ -74,13 +75,13 @@ int	set_up_heredoc(t_redir *redir, t_data *d)
 	int		fd;
 	char	*fd_name;
 
-	fd_name = ft_strjoin_with_free("./objs/", d->shlvl, FREE_NONE); //better way to generate random name?
+	fd_name = ft_strjoin_with_free("./objs/", d->shlvl, FREE_NONE);
 	fd = open(fd_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (fd == -1)
 		print_error_and_exit("open failure");
 	setup_heredoc_signal_handler();
 	readline_heredoc_loop(fd, redir->delimiter);
-	delete_tmp_file(fd, fd_name);
+	fd = delete_tmp_file(fd, fd_name);
 	if (signum == SIGINT)
 	{
 		d->exit_status = 1;
